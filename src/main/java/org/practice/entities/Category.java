92a45a4fc8c731526577ebdd80143f34cb411d8f
Umair Tariq <umairdev010@gmail.com>
2026-02-04 04:42:29 +0500

    adding chnages and updating project on wensday

--- src/main/java/org/practice/entities/Category.java
+++ src/main/java/org/practice/entities/Category.java
@@ -1,4 +1,31 @@
 package org.practice.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories_inventory")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
