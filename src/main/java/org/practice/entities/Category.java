964c091aeca0e33292f487eccd33a057dde3dbf9
Umair Tariq <umairdev010@gmail.com>
2026-02-03 17:35:41 +0500

    starting new project Simple Inventory System on tuseday

---
+++ src/main/java/org/practice/entities/Category.java
@@ -0,0 +1,4 @@
+package org.practice.entities;
+
+public class Category {
+}
---
+++ src/main/java/org/practice/umair/App.java
@@ -0,0 +1,11 @@
+package org.practice.umair;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+    }
+}
